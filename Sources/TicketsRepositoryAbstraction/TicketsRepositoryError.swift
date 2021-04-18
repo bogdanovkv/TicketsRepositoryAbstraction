0edef4d709098e36e8ef70be9198178e7b94f466
Konstatin Bogdanov <>
2021-04-18 13:46:26 +0300

    add TicketsRepositoryError

---
+++ Sources/TicketsRepositoryAbstraction/TicketsRepositoryError.swift
@@ -0,0 +1,13 @@
+//
+//  TicketsRepositoryError.swift
+//  
+//
+//  Created by Константин Богданов on 18.04.2021.
+//
+
+import Foundation
+
+enum TicketsRepositoryError: Error {
+	case urlError
+	case nilData
+}
